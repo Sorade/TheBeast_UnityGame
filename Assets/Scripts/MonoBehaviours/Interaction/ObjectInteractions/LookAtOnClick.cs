c6a913643bbc076d0e53e7808c0cea5b241e8a6d
Sorade <sorade3122@hotmail.fr>
2017-06-03 17:17:08 +0100

    Start of Building interior interactions
    Snuff Candle
    Open Book

--- Assets/Scripts/MonoBehaviours/Interaction/ObjectInteractions/LookAtOnClick.cs
+++ Assets/Scripts/MonoBehaviours/Interaction/ObjectInteractions/LookAtOnClick.cs
@@ -67,6 +67,7 @@ public class LookAtOnClick : MonoBehaviour {
 			rotCovered += Time.deltaTime * rotateSpeed;
 			Vector3 relativePosition =  trans.position - secondaryCam.GetComponent<Transform> ().position;
 			targetRotation = Quaternion.LookRotation (relativePosition, upDirection);
+			targetRotation = Quaternion.LookRotation (relativePosition, trans.position.normalized);
 			secondaryCam.GetComponent<Transform> ().rotation = Quaternion.Slerp (startRotation, targetRotation, rotCovered);
 			//Debug.Log("rot" + (secondaryCam.GetComponent<Transform> ().rotation != targetRotation));
 
