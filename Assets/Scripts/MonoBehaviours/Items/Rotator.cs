095b1c18353166cb6f7716ee1df19b8ade5a30a8
Sorade <sorade3122@hotmail.fr>
2017-09-23 15:31:39 +0100

    Added cogs and rotation mechanisms - no interaction yet

---
+++ Assets/Scripts/MonoBehaviours/Items/Rotator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	public float speed;
+	public Vector3 axis;
+	public bool isRotating = false;
+	private Vector3 initialPos;
+	private Quaternion initialRot;
+
+	void Start(){
+		initialPos = transform.position;
+		initialRot = transform.rotation;
+	}
+
+	void Update()
+	{
+		if (isRotating) {
+			// Rotate the object around the specified local axis
+			transform.Rotate(axis * Time.deltaTime * speed);
+		}
+	}
+
+	public void SwitchRotation(){
+		isRotating = !isRotating;
+		transform.position = initialPos;
+		transform.rotation = initialRot;
+	}
+}
